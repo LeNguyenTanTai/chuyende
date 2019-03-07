83f8592c02f6943ef1b4e67dbe2dfed76abf8786
Lam Trần <32736917+tranthilamm1998@users.noreply.github.com>
2019-03-08 00:01:47 +0700

    bai 2

---
+++ cdptw_verson2/cdptw/app/Airline.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Airline extends Model
+{
+    //
+    protected $table = 'airline';
+    public function countries()
+    {
+        return $this->belongsTo('App\Country');
+
+    }
+    public function getAllCountry()
+    {
+        return Country::all();
+
+    }
+}
---
+++ cdptw_verson2/cdptw/app/Country.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Country extends Model
+{
+    //
+    protected $table = 'countries';
+    // du lieu dien vao
+    protected $fillable = ['id','name','code'];
+
+    public function cities()
+    {
+        return $this->hasMany('App\City');
+    }
+    public function airline()
+    {
+
+        return $this->hasMany('App\Airline');
+
+    }
+
+    public function addCountry($data){
+        Country::insert([
+            'name' => $data->name,
+            'code' => $data->code,
+        ]);
+    }
+    public function editCountry($data){
+        Country::update([
+            'name' => $data->name,
+            'code' => $data->code,
+        ]);
+    }
+    public function deleteCountry($data){
+        if (count($data->cities) > 0) {
+
+            return redirect('countries')->with('message','Mã này đang được sử dụng ở bảng khác');
+        }
+        $data->delete();
+    }
+}
